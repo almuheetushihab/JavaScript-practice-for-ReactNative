0a3d3ff8c60efb9839895b09b64f097f45a6afd4
Almuheetu shihab <almuheetu@gmail.com>
2025-07-31 11:32:43 +0600

    complete: Variable practice & stared data Types

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-alert("I' am JavaScript!");
-alert("I' am Second javaScript Aleart");
-[1, 2, 3, 4, 5, 6].forEach(alert);
+// alert("I' am JavaScript!");
+// alert("I' am Second javaScript Aleart");
+// [1, 2, 3, 4, 5, 6].forEach(alert);
 
 // Variable
 
@@ -11,20 +11,47 @@ alert("I' am Second javaScript Aleart");
 // Using let
 // Using const
 
-var x = 5;
-var y = 6;
-var z = x + y;
-console.log(z);
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// console.log(z);
 
-let user = 'John', age = 25, message = 'Hello'
+// let user = 'John', age = 25, message = 'Hello'
 
-console.log("my user name is", user);
+// console.log("my user name is", user);
 
-let messages;
- messages = "Hello!";
- messages = "world!";
+// let messages;
+//  messages = "Hello!";
+//  messages = "world!";
 
- alert(messages)
- 
- console.log(messages);
- 
+//  alert(messages)
+
+//  console.log(messages);
+
+// let hello = 'Hello world!';
+// let secondMessage = hello;
+
+
+// alert(hello);
+// alert(secondMessage);
+
+// console.log(hello);
+// console.log(secondMessage);
+
+
+// const myBirthday = '1st January 2000';
+
+// myBirthday = '2nd January 2000';
+// console.log(myBirthday);
+
+// Uppercase constants
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_BLUE;
+alert(color); // #00F
+console.log(color); // #00F
