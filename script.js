c8d4db8bc7aae0ccb8c2da590bd418b9293dacfe
Almuheetu shihab <almuheetu@gmail.com>
2025-07-31 11:16:23 +0600

    practice: variable

---
+++ script.js
@@ -0,0 +1,30 @@
+alert("I' am JavaScript!");
+alert("I' am Second javaScript Aleart");
+[1, 2, 3, 4, 5, 6].forEach(alert);
+
+// Variable
+
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+let user = 'John', age = 25, message = 'Hello'
+
+console.log("my user name is", user);
+
+let messages;
+ messages = "Hello!";
+ messages = "world!";
+
+ alert(messages)
+ 
+ console.log(messages);
+ 
